e071f162244ca56f0f33a9eb0375c046bd94a69a
syedubaid <syedabuubaid@gmail.com>
2024-03-24 01:11:24 +0530

    added local storage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 const form=document.getElementById("formDialog")
 const add=document.getElementById("add")
+const submit=document.getElementById("submit")
 
 add.addEventListener('click',()=>{
     form.showModal();
 })
+
+
+
+function store(){
+        //getting values from dialog
+        const title=document.getElementById("form-title").value;
+        const details=document.getElementById("form-text").value;
+
+        //storing values in local storage
+        const titleStore=localStorage.setItem("form-title",title);
+        const detailsStore=localStorage.setItem("form-text",details);
+ 
+}
+submit.addEventListener('click',store());
+    
+
+
 
 class todolist{
     constructor(Date,textcontent){
