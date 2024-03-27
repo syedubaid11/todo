494e57d55a7847d09826da3ef25d0e315dda4990
syedubaid <syedabuubaid@gmail.com>
2024-03-27 22:42:05 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,33 +2,20 @@ const form=document.getElementById("formDialog")
 const add=document.getElementById("add")
 const submit=document.getElementById("submit")
 
+
+
 add.addEventListener('click',()=>{
     form.showModal();
 })
 
-
 submit.addEventListener('click',()=>{
     //getting values from dialog
     const title=document.getElementById("form-title").value;
-    const details=document.getElementById("form-text").value;
-
     //storing values in local storage
     localStorage.setItem("form-title",title);
-    localStorage.setItem("form-text",details);
 
 })
 
-
-    
-
-
-
-
-class todolist{
-    constructor(Date,textcontent){
-        this.Date=Date;
-        this.textcontent=textcontent;
-    }
-}
-
+const todocards=document.getElementById('displaytodo');
+todocards.innerHTML=localStorage.getItem("form-title");
 
