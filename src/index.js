5c79338b3cc8ad39283fac0cc6bb9d5475532df0
syedubaid <syedabuubaid@gmail.com>
2024-03-30 22:12:13 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -10,22 +10,11 @@ add.addEventListener('click',()=>{
     form.showModal();
 })
 
-let itemsArray = localStorage.getItem('form-title') ? JSON.parse(localStorage.getItem('form-title')) : [];
-
-itemsArray.forEach(addTask);
-function addTask(text){
-    const displaytodo=document.createElement('li')
-    displaytodo.textContent=text;
-}
-
-
-
 submit.addEventListener('click',()=>{
     //getting values from dialog
     const title=document.getElementById("form-title").value;
     //storing values in local storage
     localStorage.setItem("form-title",title);
-
     let text=localStorage.getItem("form-title");
 })
 
@@ -33,6 +22,7 @@ remove.addEventListener('click',()=>{
     localStorage.removeItem("form-title");
     console.log("button is pressed")
 })
+
 
 const todocards=document.getElementById('displaytodo');
 todocards.innerHTML=localStorage.getItem("form-title");
