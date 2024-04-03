bb496508b8642ae13d3f9586db6653e3ef88e4d0
syedubaid <syedabuubaid@gmail.com>
2024-04-03 16:02:34 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import addlist from "./add.js";
 
-const form=document.getElementById("formDialog")
-const add=document.getElementById("add")
-const submit=document.getElementById("submit")
-const remove=document.getElementById("remove")
+const add=document.getElementById("add-btn")
 
 let itemsArray =["hello","check"];
 const key=itemsArray[0];
 
 console.log(key)
 
-
 add.addEventListener('click',()=>{
-    form.showModal();
-})
-
-submit.addEventListener('click',()=>{
     //getting values from dialog
-    const title=document.getElementById("form-title").value;
+    const title=document.querySelector("input").value;
     //storing values in local storage
     localStorage.setItem("form-title",title);
 
@@ -27,9 +19,6 @@ submit.addEventListener('click',()=>{
 
 
 
-remove.addEventListener('click',()=>{
-    localStorage.removeItem(key);
-    console.log("button is pressed")
-})
+
 
 
